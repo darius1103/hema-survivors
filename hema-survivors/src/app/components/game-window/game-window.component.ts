f7a4a71dc7850f2d7edb9df9695d1e132f6e9e2a
Darius <darius_1103@yahoo.com>
2024-04-24 18:37:01 +0200

    Basic canvas, no interaction

--- hema-survivors/src/app/components/game-window/game-window.component.ts
+++ hema-survivors/src/app/components/game-window/game-window.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { consumerPollProducersForChange } from '@angular/core/primitives/signals';
+import { Color } from '../../utils/color';
 
 @Component({
   selector: 'app-game-window',
@@ -8,5 +10,46 @@ import { Component } from '@angular/core';
   styleUrl: './game-window.component.scss'
 })
 export class GameWindowComponent {
+  @ViewChild('canvas') canvas: any = null;
+  WIDTH = 500;
+  HEIGHT = 500;
+  ctx: any;
+  delay = 120; // 120 is about 1 frame per second :)
+  currentFrame = 0;
 
+  ngAfterViewInit() {
+    if (this.canvas == null) {
+        console.log('null canvas...')
+        return;
+    }
+    this.canvas.nativeElement.width = this.WIDTH;
+    this.canvas.nativeElement.height = this.HEIGHT;
+    console.log(this.canvas);
+
+    this.ctx = this.canvas.nativeElement.getContext("2d");
+    this.startGame();
+  }
+
+  private startGame(): void {
+    this.drawWater();
+    window.requestAnimationFrame(() => this.animate());
+  }
+
+  private drawWater(): void {
+    this.ctx.beginPath();
+    this.ctx.rect(0, 0, this.HEIGHT, this.WIDTH);
+    this.ctx.fillStyle = Color.WATER;
+    this.ctx.fill();
+  }
+
+  private animate(): void {
+    if (this.currentFrame < this.delay) {
+        this.currentFrame++;
+        window.requestAnimationFrame(() => this.animate());
+    } else {
+      console.log("Ha ha ha!");
+      this.currentFrame = 0;
+      window.requestAnimationFrame(() => this.animate());
+    }
+  }
 }
---
+++ hema-survivors/src/app/utils/color.ts
@@ -0,0 +1,9 @@
+export enum Color  {
+    WATER = '#34abeb',
+    DUCK  = '#fceb03',
+    PEAK = '#fcb603',
+    EYE = 'black',
+    SHADOW = '#03cefc',
+    HEART = '#fc0324',
+    WHITE = 'white'
+  }
